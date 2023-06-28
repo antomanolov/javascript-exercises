25eee387aa4968207160b0c765d27e34d37c7a7a
Anton Manolov <mramanolov@gmail.com>
2023-06-28 14:10:55 +0300

    completed 10th task

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(nnum) {
+    if (nnum <= 0) {
+        return 'OOPS'
+    }
+    
+    let arr = [1, 1]
+    for(let i = 0; i < nnum - 2; i++){
+        arr.push(arr[i] + arr[i+1])
+    }
+    return Number(arr.slice(-1).join(''))
 };
 
 // Do not edit below this line
