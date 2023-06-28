7143163750515d235bced0d875849f0bf10b80d2
Anton Manolov <mramanolov@gmail.com>
2023-06-28 12:31:14 +0300

    completed 8th task

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	if(arr.length === 0) {
+    return 0
+  }
+  const sum = arr.reduce((prevVal, nextVal) => {
+    return prevVal + nextVal;
+  });
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(...arr) {
+  const sum = arr.reduce((prevVal, nextVal) => {
+    return prevVal * nextVal;
+  });
+  return sum
 };
 
-const power = function() {
-	
+const power = function(num, pwr) {
+	return num ** pwr
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 || num === 1){
+    return 1
+  }
+  let sum = 1
+  for (let i = 1; i <= num; i++){
+    sum *= i
+  }
+  return sum
 };
 
 // Do not edit below this line
