c2394759e75ec030374c000030b103c5255ce895
Anton Manolov <mramanolov@gmail.com>
2023-06-28 13:51:32 +0300

    completed 9th task

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str)  {
+    let newString = str.replace(/[^a-zA-Z0-9]+/g,'').toLowerCase().split('')
+    let reverseString = str.replace(/[^a-zA-Z0-9]+/g,'').toLowerCase().split('').reverse()
+    let joinedStr = newString.join('')
+    let joinedReversed = reverseString.join('')
+    console.log(joinedReversed)
+    console.log(joinedStr)
+    return joinedReversed == joinedStr
 };
 
 // Do not edit below this line
