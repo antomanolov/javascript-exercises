3c89cfd4a208d96c61dd7a4b2dc28fe5a97e1a33
Anton Manolov <mramanolov@gmail.com>
2023-06-28 15:46:57 +0300

    completed 12th task

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let oldest = arr.sort((prevObj, nextObj) => {
+        let prevYears = getAge(prevObj.yearOfDeath, prevObj.yearOfBirth)
+        let nextYears = getAge(nextObj.yearOfDeath, nextObj.yearOfBirth)
+        return prevYears > nextYears ? -1 : 1
+      })
+      return oldest[0]
 };
 
+function getAge(death, birth) {
+    if(!death) {
+        return 2023 - birth
+    }
+    return death - birth
+}
 // Do not edit below this line
 module.exports = findTheOldest;
